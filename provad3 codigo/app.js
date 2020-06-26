73fea18176245a8a3cc94b5cbd791a00460f040a
AnneSilva <61858754+AynneSilva@users.noreply.github.com>
2020-06-25 23:08:11 -0300

    att

--- provad3 codigo/app.js
+++ provad3 codigo/app.js
@@ -3,8 +3,7 @@ function getRequest() {
     console.log('Carregou')
     axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(function (response) {
-        // handle success
-        console.log(response);
+            mountPosts(response.data)
         })
         .catch(function (error) {
         // handle error
@@ -16,11 +15,28 @@ function getRequest() {
   
 }
 
+function mountPosts(data) {
+    var postItem = ""
+
+    data.forEach(element => {
+        postItem += `<div>
+                        <h4>Título</h4>
+                        <h2>${element.title}</h2>
+                        <h5>Descricao</h5>
+                        <p>${element.body}</p>
+                        <br/>
+                    </div>`
+    });
+
+
+    document.getElementById('dataList').innerHTML = postItem;
+}
+
 function postData(titulo, descricao) {
     
     axios.post('https://jsonplaceholder.typicode.com/posts', {
-        titulo: titulo,
-        descricao: descricao
+        title: titulo,
+        body: descricao
       })
       .then(function (response) {
         console.log(response);
